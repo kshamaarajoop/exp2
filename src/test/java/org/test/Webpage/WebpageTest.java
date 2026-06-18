94be5bbe75dec7936be4db4f75b3936fbf476998
kshamaarajoop <kshamaarajoop@outlook.com>
2026-06-18 20:21:22 +0530

    title test

--- src/test/java/org/test/Webpage/WebpageTest.java
+++ src/test/java/org/test/Webpage/WebpageTest.java
@@ -1,18 +1,30 @@
 package org.test.Webpage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 import static org.testng.Assert.assertTrue;
 public class WebpageTest {
-    prive static WebDriver driver;
+    private static WebDriver driver;
     @BeforeTest
     public void openBrowser() throws InterruptedException{
-        driver=new FirefoxDriver();
+        driver=new ChromeDriver();
         driver.manage().window().maximize();
         Thread.sleep(2000);
-        driver.get("")
+        driver.get("https://kshamaarajoop.github.io/exp2/");
+    }
+    @Test
+    public void TitleValidationTest(){
+        String actualTitle= driver.getTitle();
+        String expectedTitle = "Ecommerce Demo";
+        Assert.assertEquals(actualTitle,expectedTitle);
+        assertTrue(true,"Title should be such and such");
+    }
+    @AfterTest
+    public void closeBrowser() throws InterruptedException{
+        Thread.sleep(10000);
+        driver.quit();
     }
 }
